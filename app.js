e86b987ade2b875955f3c3d327ac4bd291753112
Chandrasekar Gokulanathan <g.chandrasekar1993@gmail.com>
2018-08-19 23:00:22 +0530

    Front End Dev

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var fileUpload = require("express-fileupload");
 var indexRouter = require('./routes/index');
 var visionRouter = require('./routes/vision');
 
@@ -13,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(fileUpload());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,17 +26,6 @@ app.use('/api/vision', visionRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
--- routes/vision.js
+++ routes/vision.js
@@ -3,21 +3,35 @@ var router = express.Router();
 const vision = require("@google-cloud/vision");
 const client = new vision.ImageAnnotatorClient();
 
-router.get("/", function(req, res, next) {
-  client
-    .documentTextDetection("./resources/flipkart.png")
-    .then(results => {
-      res.json({
-        isSuccess: true,
-        data: results
+router.post("/", function(req, res, next) {
+  if (!req.files) {
+    console.log("No file found!!");
+    return res.json({ isSuccess: false, error: "No files were uploaded." });
+  }
+
+  let visionImage = req.files.visionImage;
+  visionImage.mv("./resources/visionImage.png", function(err) {
+    console.log("In file handler");
+    if (err) {
+      console.log("server error");
+      return res.json({ isSuccess: false, error: "Server Error occured!" });
+    }
+    console.log("File saved");
+    client
+      .documentTextDetection("./resources/visionImage.png")
+      .then(results => {
+        res.json({
+          isSuccess: true,
+          data: results
+        });
+      })
+      .catch(err => {
+        res.json({
+          isSuccess: false,
+          error: err
+        });
       });
-    })
-    .catch(err => {
-      res.json({
-        isSuccess: false,
-        error: err
-      });
-    });
+  });
 });
 
 module.exports = router;
